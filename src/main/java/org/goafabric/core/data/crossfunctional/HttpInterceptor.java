860cb02e87850bb7f4e4cbc8521b7a95f5955dca
Andreas Mautsch <amautsch>
2023-06-17 09:55:21 +0200

    practice data added

--- src/main/java/org/goafabric/core/data/crossfunctional/HttpInterceptor.java
+++ src/main/java/org/goafabric/core/data/crossfunctional/HttpInterceptor.java
@@ -1,37 +1,47 @@
 package org.goafabric.core.data.crossfunctional;
 
+import io.micrometer.common.KeyValue;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.filter.ServerHttpObservationFilter;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 public class HttpInterceptor implements WebMvcConfigurer {
     private static final ThreadLocal<String> tenantId = new ThreadLocal<>();
-    private static final ThreadLocal<String> userName = new ThreadLocal<>();
 
-    /*
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new HandlerInterceptor() {
             @Override
             public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-                //tenantId.set(request.getHeader("X-TenantId"));
-                //userName.set(request.getHeader("X-Auth-Request-Preferred-Username"));
+                configureLogsAndTracing(getTenantId(), request);
                 return true;
             }
 
             @Override
             public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
                 tenantId.remove();
-                userName.remove();
+                MDC.remove("tenantId");
             }
+
+            private static void configureLogsAndTracing(String tenantId, HttpServletRequest request) {
+                if (tenantId != null && request != null) {
+                    MDC.put("tenantId", tenantId);
+                    ServerHttpObservationFilter.findObservationContext(request).ifPresent(
+                            context -> context.addHighCardinalityKeyValue(KeyValue.of("tenantId.id", tenantId)));
+                }
+            }
+
         });
     }
-
-     */
-
 
     public static void setTenantId(String tenantId) {
         HttpInterceptor.tenantId.set(tenantId);
@@ -44,8 +54,7 @@ public class HttpInterceptor implements WebMvcConfigurer {
     }
 
     public static String getUserName() {
-        return userName.get() != null ? userName.get()
-                : SecurityContextHolder.getContext().getAuthentication() != null ? SecurityContextHolder.getContext().getAuthentication().getName() : "";
+        return SecurityContextHolder.getContext().getAuthentication() != null ? SecurityContextHolder.getContext().getAuthentication().getName() : "";
     }
 
     public static String getCompanyId() {
