0c8d936f635478f373efd4b06aa66ee3a655e017
Andreas Mautsch <amautsch>
2023-06-26 11:23:54 +0200

    vaadin added

---
+++ src/test/java/org/goafabric/core/files/importexport/logic/ExportLogicIT.java
@@ -0,0 +1,24 @@
+package org.goafabric.core.files.importexport.logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ExportLogicIT {
+    @Autowired
+    private ExportLogic exportLogic;
+
+    @Autowired
+    private ImportLogic importLogic;
+
+    @Test
+    void export() {
+        var tempDir = System.getProperty("java.io.tmpdir");
+
+        exportLogic.run(tempDir);
+        importLogic.run(tempDir);
+    }
+}
