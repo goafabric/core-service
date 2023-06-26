95f6a02f0e81eb3f15b53169cdc80ebbfe4f570d
Andreas Mautsch <amautsch>
2023-06-26 15:17:55 +0200

    mem increased

--- src/main/java/org/goafabric/core/ui/adapter/DiagnosisAdapter.java
+++ src/main/java/org/goafabric/core/ui/adapter/DiagnosisAdapter.java
@@ -1,6 +1,8 @@
 package org.goafabric.core.ui.adapter;
 
 import org.goafabric.core.ui.adapter.dto.Diagnosis;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -17,9 +20,18 @@ public class DiagnosisAdapter {
     @Autowired private RestTemplate restTemplate;
 
     @Value("${frontend.catalog-service.uri}") private String uri;
-    
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     public List<Diagnosis> findByDisplay(String search) {
-        return restTemplate.exchange(uri + "/diagnosis/findByDisplay?display={display}", HttpMethod.GET, null,
-                new ParameterizedTypeReference<List<Diagnosis>>(){}, search).getBody();
+        try {
+            return restTemplate.exchange(uri + "/diagnosis/findByDisplay?display={display}", HttpMethod.GET, null,
+                    new ParameterizedTypeReference<List<Diagnosis>>() {
+                    }, search).getBody();
+        } catch (Exception e) {
+            log.warn("exception during rest: " + e.getMessage());
+            return new ArrayList<>();
+        }
     }
+
 }
