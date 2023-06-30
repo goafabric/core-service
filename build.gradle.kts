2d0707b0dc7d99de54124080955d3ed835d94d43
Andreas Mautsch <amautsch>
2023-06-30 09:42:38 +0200

    release version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 group = "org.goafabric"
-version = "1.0.2-vaadin-SNAPSHOT"
+version = "1.0.2-vaadin"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 val dockerRegistry = "goafabric"
