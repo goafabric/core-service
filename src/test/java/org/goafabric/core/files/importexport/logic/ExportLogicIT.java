0e9a0eece670c0b5ea3ecd071029c85ac90e31e7
Andreas Mautsch <amautsch>
2023-07-04 08:54:31 +0200

    changes

--- src/test/java/org/goafabric/core/files/importexport/logic/ExportLogicIT.java
+++ src/test/java/org/goafabric/core/files/importexport/logic/ExportLogicIT.java
@@ -1,11 +1,28 @@
 package org.goafabric.core.files.importexport.logic;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.goafabric.core.crossfunctional.TenantInterceptor;
+import org.goafabric.core.data.controller.PatientController;
+import org.goafabric.core.data.controller.vo.Patient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.goafabric.core.DataRocker.*;
+
 @SpringBootTest
 public class ExportLogicIT {
+    @Autowired
+    private PatientController controller;
+
     @Autowired
     private ExportLogic exportLogic;
 
@@ -13,10 +30,31 @@ public class ExportLogicIT {
     private ImportLogic importLogic;
 
     @Test
-    void export() {
+    void export() throws IOException {
+        setTenantId("0");
+        var id = create();
+
         var tempDir = System.getProperty("java.io.tmpdir");
 
         exportLogic.run(tempDir);
         importLogic.run(tempDir);
+
+        var patients = new ObjectMapper().registerModule(new JavaTimeModule()).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .registerModule(new JavaTimeModule()).readValue(new File(tempDir + "/patient.json"), new TypeReference<List<Patient>>() {});
+
+        assertThat(patients).isNotNull().isNotEmpty();
+        delete(id);
+    }
+
+    private String create() {
+        return controller.save(
+                createPatient("Homer", "Simpson",
+                        createAddress("Evergreen Terrace " + TenantInterceptor.getTenantId()),
+                        createContactPoint("555-444"))
+        ).id();
+    }
+
+    private void delete(String id) {
+        controller.deleteById(id);
     }
 }
