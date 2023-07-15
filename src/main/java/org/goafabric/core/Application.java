0bf4b7439ec0cd4ea3b9f583509dc24b27c6afc0
Andreas Mautsch <amautsch>
2023-07-15 07:17:17 +0200

    changes

--- src/main/java/org/goafabric/core/Application.java
+++ src/main/java/org/goafabric/core/Application.java
@@ -1,8 +1,11 @@
 package org.goafabric.core;
 
 import io.awspring.cloud.autoconfigure.core.AwsAutoConfiguration;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 
 /**
@@ -14,6 +17,13 @@ public class Application {
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner init(ApplicationContext context) {
+        return args -> {
+            if ((args.length > 0) && ("-check-integrity".equals(args[0]))) { SpringApplication.exit(context, () -> 0);}
+        };
     }
 
 }
