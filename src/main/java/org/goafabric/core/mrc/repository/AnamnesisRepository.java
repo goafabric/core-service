66e8910f68fee523d89e2492e097ae1ddd94b010
Andreas Mautsch <amautsch>
2023-08-08 16:47:03 +0200

    encounter added

--- src/main/java/org/goafabric/core/mrc/repository/AnamnesisRepository.java
+++ src/main/java/org/goafabric/core/mrc/repository/AnamnesisRepository.java
@@ -2,6 +2,8 @@ package org.goafabric.core.mrc.repository;
 
 
 import org.goafabric.core.mrc.repository.entity.AnamnesisEo;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,13 +12,12 @@ import java.util.List;
 public interface AnamnesisRepository extends CrudRepository<AnamnesisEo, String> {
     List<AnamnesisEo> findByEncounterIdAndTextContainsIgnoreCase(String encounterId, String text);
 
+    @Query("select a from AnamnesisEo a")
+    List<AnamnesisEo> findAllByEncounterId(String encounterId, TextCriteria criteria);
+
     /*
     @Query(nativeQuery = true, value = "select text from observation WHERE to_tsvector('english', text) @@ to_tsquery('english', :criteria)")
     List<String> findByText(String criteria);
-
-    @Query("select o from ObservationEo o")
-    List<AnamnesisEo> findAllBy(TextCriteria criteria);
-
      */
 }
 
