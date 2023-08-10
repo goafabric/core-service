cad6da209da1140f5c37b4bca91f45f4b4617c72
Andreas Mautsch <amautsch>
2023-08-10 12:17:14 +0200

    changes

--- src/main/java/org/goafabric/core/mrc/repository/AnamnesisRepository.java
+++ src/main/java/org/goafabric/core/mrc/repository/AnamnesisRepository.java
@@ -10,14 +10,11 @@ import java.util.List;
 
 // TODO: use gin index + computated field, check if extension is needed, @@ phraseto_tsquery
 public interface AnamnesisRepository extends CrudRepository<AnamnesisEo, String> {
+    //@Query(nativeQuery = true, value = "select * from anamnesis WHERE encounter_id = :encounterId and to_tsvector('english', text) @@ to_tsquery('english', :text)")
     List<AnamnesisEo> findByEncounterIdAndTextContainsIgnoreCase(String encounterId, String text);
 
     @Query("select a from AnamnesisEo a")
     List<AnamnesisEo> findAllByEncounterId(String encounterId, TextCriteria criteria);
 
-    /*
-    @Query(nativeQuery = true, value = "select text from observation WHERE to_tsvector('english', text) @@ to_tsquery('english', :criteria)")
-    List<String> findByText(String criteria);
-     */
 }
 
--- src/main/java/org/goafabric/core/mrc/repository/ConditionRepository.java
+++ src/main/java/org/goafabric/core/mrc/repository/ConditionRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ConditionRepository extends CrudRepository<ConditionEo, String> {
+    //@Query(nativeQuery = true, value = "select * from condition WHERE encounter_id = :encounterId and to_tsvector('english', display) @@ to_tsquery('english', :display)")
     List<ConditionEo> findByEncounterIdAndDisplayContainsIgnoreCase(String encounterId, String display);
 
     @Query("select c from ConditionEo c")
