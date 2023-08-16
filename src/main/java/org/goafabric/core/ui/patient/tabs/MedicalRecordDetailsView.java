9e4148cd29b2910c58947d73bb633fdb1a0b111d
Andreas Mautsch <amautsch>
2023-08-16 14:51:13 +0200

    css added

--- src/main/java/org/goafabric/core/ui/patient/tabs/MedicalRecordDetailsView.java
+++ src/main/java/org/goafabric/core/ui/patient/tabs/MedicalRecordDetailsView.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import org.goafabric.core.mrc.repository.entity.MedicalRecordEo;
+import org.h2.util.StringUtils;
 
 public class MedicalRecordDetailsView extends Dialog {
     public MedicalRecordDetailsView(MedicalRecordEo medicalRecord) {
@@ -12,9 +13,11 @@ public class MedicalRecordDetailsView extends Dialog {
         var layout = new VerticalLayout();
         var display = new TextField("Display", medicalRecord.display);
         display.setWidth("500px");
-
         layout.add(display);
-        layout.add(new TextField("Code", medicalRecord.code));
+
+        if (!StringUtils.isNullOrEmpty(medicalRecord.code)) {
+            layout.add(new TextField("Code", medicalRecord.code));
+        }
         this.add(layout);
     }
 }
