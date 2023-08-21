fa79e82d9fa4b5cbcc0b354c9caddd36c4f3d6bf
Andreas Mautsch <amautsch>
2023-08-21 16:02:03 +0200

    changes

--- src/main/java/org/goafabric/core/data/repository/extensions/AuditEventDispatcher.java
+++ src/main/java/org/goafabric/core/data/repository/extensions/AuditEventDispatcher.java
@@ -1,7 +1,6 @@
 package org.goafabric.core.data.repository.extensions;
 
 import org.goafabric.core.extensions.HttpInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -17,17 +16,16 @@ import java.util.concurrent.Executors;
 
 @Component
 public class AuditEventDispatcher {
-    @Autowired
-    private RestTemplate auditRestTemplate;
-
-    @Value("${event.dispatcher.uri:}")
-    private String eventDispatcherUri;
-
+    private final RestTemplate auditRestTemplate;
+    private final String eventDispatcherUri;
     private final ExecutorService executor = Executors.newFixedThreadPool(3);
 
-    record ChangeEvent (String id, String tenantId, String referenceId, String type, AuditTrailListener.DbOperation operation, String origin) {}
+    public AuditEventDispatcher(RestTemplate auditRestTemplate, @Value("${event.dispatcher.uri:}") String eventDispatcherUri) {
+        this.auditRestTemplate = auditRestTemplate;
+        this.eventDispatcherUri = eventDispatcherUri;
+    }
 
-    public  void dispatchEvent(AuditTrailListener.AuditTrail auditTrail) {
+    public void dispatchEvent(AuditTrailListener.AuditTrail auditTrail) {
         if (!eventDispatcherUri.isEmpty()) {
             var changeEvent = new ChangeEvent(auditTrail.id(), HttpInterceptor.getTenantId(), auditTrail.objectId(), auditTrail.objectType(), auditTrail.operation(), "core");
             var headers = new HttpHeaders();
@@ -41,4 +39,6 @@ public class AuditEventDispatcher {
     public RestTemplate auditRestTemplate(RestTemplateBuilder builder) {
         return builder.setConnectTimeout(Duration.ofMillis(1000)).setReadTimeout(Duration.ofMillis(1000)).build();
     }
+
+    record ChangeEvent (String id, String tenantId, String referenceId, String type, AuditTrailListener.DbOperation operation, String origin) {}
 }
