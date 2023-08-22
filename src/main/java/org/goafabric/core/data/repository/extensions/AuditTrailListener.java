59b1792d08a05678e0fed7cd9b3e7c113bf3235b
Andreas Mautsch <amautsch>
2023-08-22 07:50:02 +0200

    audit fix

--- src/main/java/org/goafabric/core/data/repository/extensions/AuditTrailListener.java
+++ src/main/java/org/goafabric/core/data/repository/extensions/AuditTrailListener.java
@@ -9,10 +9,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
@@ -21,6 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import javax.sql.DataSource;
+import java.time.Duration;
 import java.util.Date;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
@@ -30,9 +37,11 @@ import java.util.concurrent.Executors;
 public class AuditTrailListener implements ApplicationContextAware {
     private static ApplicationContext context;
 
-    enum DbOperation { CREATE, READ, UPDATE, DELETE }
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    private record AuditTrail(
+    private enum DbOperation { CREATE, READ, UPDATE, DELETE }
+
+    record AuditTrail(
             String id,
             String orgunitId,
             String objectType,
@@ -51,131 +60,113 @@ public class AuditTrailListener implements ApplicationContextAware {
         context = applicationContext;
     }
 
-
     @PostPersist
     public void afterCreate(Object object)  {
-        context.getBean(AuditProcessor.class).afterCreate(object);
+        insertAudit(DbOperation.CREATE,  getId(object), null, object);
     }
 
     @PostUpdate
     public void afterUpdate(Object object) {
-        context.getBean(AuditProcessor.class).afterUpdate(object);
+        final String id = getId(object);
+        insertAudit(DbOperation.UPDATE, id,
+                context.getBean(AuditJpaUpdater.class).findOldObject(object.getClass(), id), object);
     }
 
     @PostRemove
     public void afterDelete(Object object) {
-        context.getBean(AuditProcessor.class).afterDelete(object);
+        insertAudit(DbOperation.DELETE, getId(object), object, null);
     }
 
+    private void insertAudit(final DbOperation operation, String referenceId, final Object oldObject, final Object newObject) {
+        try {
+            var auditTrail = createAuditTrail(operation, referenceId, oldObject, newObject);
+            log.debug("New audit:\n{}", auditTrail);
+            context.getBean(AuditJpaInserter.class).insertAudit(auditTrail, oldObject != null ? oldObject : newObject);
+        } catch (Exception e) {
+            log.error("Error during audit:\n{}", e.getMessage(), e);
+        }
+    }
+
+    private AuditTrail createAuditTrail(
+            DbOperation dbOperation, String referenceId, final Object oldObject, final Object newObject) throws JsonProcessingException {
+        final Date date = new Date(System.currentTimeMillis());
+        return new AuditTrail(
+                UUID.randomUUID().toString(),
+                TenantResolver.getOrgunitId(),
+                getTableName(newObject != null ? newObject : oldObject),
+                referenceId,
+                dbOperation,
+                (dbOperation == DbOperation.CREATE ? HttpInterceptor.getUserName() : null),
+                (dbOperation == DbOperation.CREATE ? date : null),
+                ((dbOperation == DbOperation.UPDATE || dbOperation == DbOperation.DELETE) ? HttpInterceptor.getUserName() : null),
+                ((dbOperation == DbOperation.UPDATE || dbOperation == DbOperation.DELETE) ? date : null),
+                (oldObject == null ? null : getJsonValue(oldObject)),
+                (newObject == null ? null : getJsonValue(newObject))
+        );
+    }
+
+    private String getJsonValue(final Object object) throws JsonProcessingException {
+        return new ObjectMapper().registerModule(new JavaTimeModule()).writerWithDefaultPrettyPrinter().writeValueAsString(object);
+    }
+
+    @Component
+    @Profile("jpa")
+    static class AuditJpaUpdater {
+        @PersistenceContext private EntityManager entityManager;
+
+        @Transactional(propagation = Propagation.REQUIRES_NEW) //new transaction helps us to retrieve the old value still inside the db
+        public <T> T findOldObject(Class<T> clazz, String id) {
+            return entityManager.find(clazz, id);
+        }
+    }
 
     @Component
     @Profile("jpa")
     @RegisterReflectionForBinding(AuditTrail.class)
-    static class AuditProcessor {
+    static class AuditJpaInserter {
         private final DataSource dataSource;
         private final String     schemaPrefix;
-        @PersistenceContext private EntityManager entityManager;
-        private static final Logger log = LoggerFactory.getLogger("org.goafabric.AuditProcessor");
 
-        private final RestTemplate auditRestTemplate;
-        private final String eventDispatcherUri;
-        private static final ExecutorService executor = Executors.newFixedThreadPool(3);
-
-        public AuditProcessor(DataSource dataSource, @Value("${multi-tenancy.schema-prefix:_}") String schemaPrefix,
-                              RestTemplate auditRestTemplate, @Value("${event.dispatcher.uri:}") String eventDispatcherUri) {
+        public AuditJpaInserter(DataSource dataSource, @Value("${multi-tenancy.schema-prefix:_}") String schemaPrefix) {
             this.dataSource = dataSource;
             this.schemaPrefix = schemaPrefix;
-            this.auditRestTemplate = auditRestTemplate;
-            this.eventDispatcherUri = eventDispatcherUri;
-        }
-
-        public void afterCreate(Object object)  {
-            context.getBean(AuditProcessor.class).insertAudit(DbOperation.CREATE,  getId(object), null, object);
-        }
-
-        public void afterUpdate(Object object) {
-            final String id = getId(object);
-            context.getBean(AuditProcessor.class).insertAudit(DbOperation.UPDATE, id,
-                    findOldObject(object.getClass(), id), object);
-        }
-
-        public void afterDelete(Object object) {
-            context.getBean(AuditProcessor.class).insertAudit(DbOperation.DELETE, getId(object), object, null);
-        }
-
-        private void insertAudit(final DbOperation operation, String referenceId, final Object oldObject, final Object newObject) {
-            try {
-                var auditTrail = createAuditTrail(operation, referenceId, oldObject, newObject);
-                log.debug("New audit:\n{}", auditTrail);
-                insertAudit(auditTrail, oldObject != null ? oldObject : newObject);
-                //dispatchEvent(auditTrail);
-            } catch (Exception e) {
-                log.error("Error during audit:\n{}", e.getMessage(), e);
-            }
-        }
-
-        private AuditTrail createAuditTrail(
-                DbOperation dbOperation, String referenceId, final Object oldObject, final Object newObject) throws JsonProcessingException {
-            final Date date = new Date(System.currentTimeMillis());
-            return new AuditTrail(
-                    UUID.randomUUID().toString(),
-                    TenantResolver.getOrgunitId(),
-                    getTableName(newObject != null ? newObject : oldObject),
-                    referenceId,
-                    dbOperation,
-                    (dbOperation == DbOperation.CREATE ? HttpInterceptor.getUserName() : null),
-                    (dbOperation == DbOperation.CREATE ? date : null),
-                    ((dbOperation == DbOperation.UPDATE || dbOperation == DbOperation.DELETE) ? HttpInterceptor.getUserName() : null),
-                    ((dbOperation == DbOperation.UPDATE || dbOperation == DbOperation.DELETE) ? date : null),
-                    (oldObject == null ? null : getJsonValue(oldObject)),
-                    (newObject == null ? null : getJsonValue(newObject))
-            );
-        }
-
-        private String getJsonValue(final Object object) throws JsonProcessingException {
-            return new ObjectMapper().registerModule(new JavaTimeModule()).writerWithDefaultPrettyPrinter().writeValueAsString(object);
         }
 
         public void insertAudit(AuditTrail auditTrail, Object object) { //we cannot use jpa because of the dynamic table name
             new SimpleJdbcInsert(dataSource)
                     .withSchemaName(schemaPrefix + HttpInterceptor.getTenantId())
                     .withTableName("audit_trail")
-                .execute(new BeanPropertySqlParameterSource(auditTrail));
+                    .execute(new BeanPropertySqlParameterSource(auditTrail));
         }
-
-        @Transactional(propagation = Propagation.REQUIRES_NEW) //new transaction helps us to retrieve the old value still inside the db
-        public <T> T findOldObject(Class<T> clazz, String id) {
-            return entityManager.find(clazz, id);
-        }
-
-        private static String getId(Object object) {
-            return String.valueOf(context.getBean(EntityManagerFactory.class).getPersistenceUnitUtil().getIdentifier(object));
-        }
-
-        private static String getTableName(Object object) {
-            return object.getClass().getSimpleName().replaceAll("Eo", "").toLowerCase();
-        }
-
-        /*
-        public void dispatchEvent(AuditTrailListener.AuditTrail auditTrail) {
-            if (!eventDispatcherUri.isEmpty()) {
-                var changeEvent = new ChangeEvent(auditTrail.id(), HttpInterceptor.getTenantId(), auditTrail.objectId(), auditTrail.objectType(), auditTrail.operation(), "core");
-                var headers = new HttpHeaders();
-                headers.setContentType(MediaType.APPLICATION_JSON);
-                executor.submit(() -> {
-                    auditRestTemplate.postForEntity(eventDispatcherUri, new HttpEntity<>(changeEvent, headers), Void.class); });
-            }
-        }
-
-        @Bean
-        public RestTemplate auditRestTemplate(RestTemplateBuilder builder) {
-            return builder.setConnectTimeout(Duration.ofMillis(1000)).setReadTimeout(Duration.ofMillis(1000)).build();
-        }
-
-        record ChangeEvent (String id, String tenantId, String referenceId, String type, AuditTrailListener.DbOperation operation, String origin) {}
-
-         */
     }
+
+    private static String getId(Object object) {
+        return String.valueOf(context.getBean(EntityManagerFactory.class).getPersistenceUnitUtil().getIdentifier(object));
+    }
+
+    private static String getTableName(Object object) {
+        return object.getClass().getSimpleName().replaceAll("Eo", "").toLowerCase();
+    }
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(3);
+    @Autowired private RestTemplate auditRestTemplate;
+    @Value("${event.dispatcher.uri:}") private String eventDispatcherUri;
+    public void dispatchEvent(AuditTrailListener.AuditTrail auditTrail) {
+        if (!eventDispatcherUri.isEmpty()) {
+            var changeEvent = new ChangeEvent(auditTrail.id(), HttpInterceptor.getTenantId(), auditTrail.objectId(), auditTrail.objectType(), auditTrail.operation(), "core");
+            var headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            executor.submit(() -> {
+                auditRestTemplate.postForEntity(eventDispatcherUri, new HttpEntity<>(changeEvent, headers), Void.class); });
+        }
+    }
+
+    @Bean
+    public RestTemplate auditRestTemplate(RestTemplateBuilder builder) {
+        return builder.setConnectTimeout(Duration.ofMillis(1000)).setReadTimeout(Duration.ofMillis(1000)).build();
+    }
+
+    record ChangeEvent (String id, String tenantId, String referenceId, String type, AuditTrailListener.DbOperation operation, String origin) {}
 
 }
 
