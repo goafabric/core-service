6830a23e02b8dd1c3e29d20e0229b9a0fb7ff244
Andreas Mautsch <amautsch>
2023-08-30 07:50:09 +0200

    version increase

--- src/main/java/org/goafabric/core/organization/logic/UserLogic.java
+++ src/main/java/org/goafabric/core/organization/logic/UserLogic.java
@@ -2,7 +2,6 @@ package org.goafabric.core.organization.logic;
 
 import org.goafabric.core.organization.controller.vo.User;
 import org.goafabric.core.organization.logic.mapper.UserMapper;
-import org.goafabric.core.organization.repository.RoleRepository;
 import org.goafabric.core.organization.repository.UserRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,12 +15,9 @@ public class UserLogic {
 
     private final UserRepository repository;
 
-    private final RoleRepository roleRepository;
-
-    public UserLogic(UserMapper mapper, UserRepository repository, RoleRepository roleRepository) {
+    public UserLogic(UserMapper mapper, UserRepository repository) {
         this.mapper = mapper;
         this.repository = repository;
-        this.roleRepository = roleRepository;
     }
 
     public User getById(String id) {
@@ -40,10 +36,9 @@ public class UserLogic {
     }
 
     public User save(User user) {
-        var userEo = mapper.map(user);
         //userEo.roles = user.roles().stream().map(role -> roleRepository.findById(role.id()).get()).toList();
-        return mapper.map(repository.save(userEo));
+        return mapper.map(repository.save(
+                mapper.map(user)));
     }
-
 
 }
