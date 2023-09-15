1c6ac4ac0ae52ace2371ba1f26a2f61413f784a4
Andreas Mautsch <amautsch>
2023-09-15 10:46:18 +0200

    broken build

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,7 +95,7 @@ jib {
 tasks.register("dockerImageNativeNoTest") {group = "build"; dependsOn("bootJar")
 	doFirst {exec { commandLine(
 		"docker", "run", "--rm", "--mount", "type=bind,source=${projectDir}/build,target=/build", "--entrypoint", "/bin/bash", graalvmBuilderImage, "-c", """ mkdir -p /build/native/nativeCompile && cp /build/libs/*-SNAPSHOT.jar /build/native/nativeCompile && cd /build/native/nativeCompile && jar -xvf *.jar &&
-		native-image -J-Xmx5000m -march=compatibility -H:Name=application --trace-class-initialization=org.apache.commons.logging.LogFactory -H:+ReportExceptionStackTraces $([[ -f META-INF/native-image/argfile ]] && echo @META-INF/native-image/argfile) -cp .:BOOT-INF/classes:$(ls -d -1 "/build/native/nativeCompile/BOOT-INF/lib/"*.* | tr "\n" ":") && /build/native/nativeCompile/application -check-integrity """
+		native-image -J-Xmx5000m -march=compatibility -H:Name=application --initialize-at-build-time=org.apache.commons.logging.LogFactory --trace-class-initialization=org.apache.commons.logging.LogFactory -H:+ReportExceptionStackTraces $([[ -f META-INF/native-image/argfile ]] && echo @META-INF/native-image/argfile) -cp .:BOOT-INF/classes:$(ls -d -1 "/build/native/nativeCompile/BOOT-INF/lib/"*.* | tr "\n" ":") && /build/native/nativeCompile/application -check-integrity """
 	)}}
 	doLast {
 		jib.from.image = "ubuntu:22.04"
