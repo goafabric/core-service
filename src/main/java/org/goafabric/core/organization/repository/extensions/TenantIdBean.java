17997d7e254f3a0cc18ace45bc408bd72fcc3f01
Andreas Mautsch <amautsch>
2023-10-10 15:40:52 +0200

    changes

--- src/main/java/org/goafabric/core/organization/repository/extensions/TenantIdBean.java
+++ src/main/java/org/goafabric/core/organization/repository/extensions/TenantIdBean.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Component;
 @RegisterReflectionForBinding(TenantIdBean.class)
 public class TenantIdBean {
     public String getPrefix() {
-        return "tenant-" + HttpInterceptor.getTenantId() + "-";
+        return "core-" + HttpInterceptor.getTenantId() + "-";
     }
 }
