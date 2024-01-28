cbeff0d8f99ac10d2b2b116e5a7d639846623baa
Andreas Mautsch <amautsch>
2024-01-28 11:09:16 +0100

    mem upgrade

--- src/main/java/org/goafabric/core/medicalrecords/logic/elastic/ElasticSearchConfiguration.java
+++ src/main/java/org/goafabric/core/medicalrecords/logic/elastic/ElasticSearchConfiguration.java
@@ -1,11 +1,13 @@
 package org.goafabric.core.medicalrecords.logic.elastic;
 
+import org.springframework.boot.actuate.autoconfigure.data.elasticsearch.ElasticsearchReactiveHealthContributorAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.elasticsearch.ElasticsearchRestHealthContributorAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("jpa")
 @Configuration
-@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration.class, org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration.class, org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration.class, org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration.class, org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration.class, ElasticsearchRestHealthContributorAutoConfiguration.class, ElasticsearchReactiveHealthContributorAutoConfiguration.class})
 public class ElasticSearchConfiguration {
 }
