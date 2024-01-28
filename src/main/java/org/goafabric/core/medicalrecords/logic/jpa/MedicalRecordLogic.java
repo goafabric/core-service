487af26c576d7056d015546bdae7beb19f9f91dd
Andreas Mautsch <amautsch>
2024-01-28 11:51:17 +0100

    changes

--- src/main/java/org/goafabric/core/medicalrecords/logic/jpa/MedicalRecordLogic.java
+++ src/main/java/org/goafabric/core/medicalrecords/logic/jpa/MedicalRecordLogic.java
@@ -27,6 +27,10 @@ public class MedicalRecordLogic implements MedicalRecordLogicAble {
     }
 
     public MedicalRecord save(MedicalRecord medicalRecord) {
+        if (medicalRecord.relation() != null && medicalRecord.id() != null) {
+            throw new IllegalStateException("Records with relations should not be updated directly");
+        }
+
         return mapper.map(
             repository.save(mapper.map(medicalRecord))
         );
