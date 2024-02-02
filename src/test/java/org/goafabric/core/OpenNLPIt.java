3890e316db8cd85a353d9a7e4fa25a3140995cc5
Andreas Mautsch <amautsch>
2024-02-02 14:36:58 +0100

    chat

--- src/test/java/org/goafabric/core/OpenNLPIt.java
+++ src/test/java/org/goafabric/core/OpenNLPIt.java
@@ -11,7 +11,7 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
-public class OpneNLPIt {
+public class OpenNLPIt {
 
     @Test
     public void test() throws IOException {
