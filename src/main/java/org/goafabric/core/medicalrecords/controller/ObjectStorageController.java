b871cf4aef06418e91d1f3be22b5df2e36442648
Andreas Mautsch <amautsch>
2024-02-26 14:00:10 +0100

    native s3

--- src/main/java/org/goafabric/core/medicalrecords/controller/ObjectStorageController.java
+++ src/main/java/org/goafabric/core/medicalrecords/controller/ObjectStorageController.java
@@ -1,7 +1,6 @@
 package org.goafabric.core.medicalrecords.controller;
 
 import org.goafabric.core.medicalrecords.controller.dto.ObjectEntry;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,37 +10,26 @@ import java.util.List;
 @RequestMapping(value = "/objects", produces = MediaType.APPLICATION_JSON_VALUE)
 @RestController
 public class ObjectStorageController {
-    private final ObjectStorageLogicNative logic;
+    private final ObjectStorageLogic logic;
 
     private final List<ObjectEntry> objectEntriesInMem = new ArrayList<>();
 
-    private final Boolean s3Enabled;
-    
-    public ObjectStorageController(ObjectStorageLogicNative logic, @Value("${spring.cloud.aws.s3.enabled}") Boolean s3Enabled) {
+    public ObjectStorageController(ObjectStorageLogic logic) {
         this.logic = logic;
-        this.s3Enabled = s3Enabled;
     }
 
     @GetMapping("getById/{name}")
     public ObjectEntry getByName(@PathVariable("name") String name) {
-        return s3Enabled
-                ? logic.getById(name)
-                : objectEntriesInMem.stream().filter(o -> o.objectName().equals(name)).findFirst().get();
+        return logic.getById(name);
     }
 
     @GetMapping("search")
     public List<ObjectEntry> search(@RequestParam("search") String search) {
-        return s3Enabled
-                ? logic.search(search)
-                : objectEntriesInMem.stream().filter(o -> o.objectName().startsWith(search)).toList();
+        return logic.search(search);
     }
 
     @PostMapping(value = "save", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void save(@RequestBody ObjectEntry objectEntry) {
-        if (s3Enabled) {
-            logic.save(objectEntry);
-        } else {
-            objectEntriesInMem.add(objectEntry);
-        }
+        logic.save(objectEntry);
     }
 }
