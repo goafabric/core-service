156210a33ddf1cfa6764c5064b9da775eee8dd0d
Andreas Mautsch <amautsch>
2024-03-05 09:55:26 +0100

    changes

--- src/main/java/org/goafabric/core/extensions/SecurityConfiguration.java
+++ src/main/java/org/goafabric/core/extensions/SecurityConfiguration.java
@@ -26,30 +26,29 @@ public class SecurityConfiguration {
     @Value("${spring.security.oauth2.logout-uri:}") private String logoutUri;
     @Value("${spring.security.oauth2.prefix:}") private String prefix;
 
+
     @Value("${spring.security.oauth2.client-id}") private String clientId;
     @Value("${spring.security.oauth2.client-secret}") private String clientSecret;
     @Value("${spring.security.oauth2.user-name-attribute:}") private String userNameAttribute;
-
-    private static final String TENANT_ID = "0"; //we are always using a fixed Registration ID here
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         if (isAuthenticationEnabled) {
             var clientRegistrationRepository = new TenantClientRegistrationRepository();
             var logoutHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-            logoutHandler.setPostLogoutRedirectUri("{baseUrl}/oauth2/authorization/" + TENANT_ID); //yeah that's right, we need baseUrl here, because it's an absolute url and below its a relative url - WTF
+            logoutHandler.setPostLogoutRedirectUri("{baseUrl}/login.html"); //yeah that's right, we need baseUrl here, because it's an absolute url and below its a relative url - WTF
             http
                     .authorizeHttpRequests(authorize -> authorize
                             .requestMatchers(new MvcRequestMatcher(introspector, "/"), new MvcRequestMatcher(introspector, "actuator/**"), new MvcRequestMatcher(introspector, "/login.html")).permitAll()
                             .anyRequest().authenticated())
                     .oauth2Login(oauth2 -> oauth2
-                            .clientRegistrationRepository(clientRegistrationRepository)
+                                    .clientRegistrationRepository(clientRegistrationRepository)
                             //.defaultSuccessUrl("/frontend/",true)
                     )
                     .logout(l -> l.logoutSuccessHandler(logoutHandler))
                     .csrf(c -> c.disable())
                     .exceptionHandling(exception ->
-                            exception.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/oauth2/authorization/" + TENANT_ID)));
+                            exception.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login.html")));
         } else {
             http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).csrf(csrf -> csrf.disable());
         }
@@ -59,10 +58,10 @@ public class SecurityConfiguration {
     class TenantClientRegistrationRepository implements ClientRegistrationRepository {
 
         private static final Map<String, ClientRegistration> clientRegistrations = new ConcurrentHashMap<>();
-        
+
         @Override
         public ClientRegistration findByRegistrationId(String registrationId) {
-            return clientRegistrations.computeIfAbsent(TENANT_ID, this::buildClientRegistration);
+            return clientRegistrations.computeIfAbsent(registrationId, this::buildClientRegistration);
         }
 
         private ClientRegistration buildClientRegistration(String tenantId) {
