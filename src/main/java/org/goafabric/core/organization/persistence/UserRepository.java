f64847e88eadb0039711bfa25b03f5cd0d55b140
Andreas Mautsch <amautsch>
2024-03-18 15:08:26 +0100

    changes

--- src/main/java/org/goafabric/core/organization/persistence/UserRepository.java
+++ src/main/java/org/goafabric/core/organization/persistence/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<UserEo, String> {
+    List<UserEo> findByName(String name);
     List<UserEo> findByNameStartsWithIgnoreCase(String name);
 }
 
