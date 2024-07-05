b00bc90516222bae14fde48175ed882cd8165c24
Andreas Mautsch <amautsch>
2024-07-05 08:00:34 +0200

    changes

--- src/main/java/org/goafabric/core/organization/controller/UserController.java
+++ src/main/java/org/goafabric/core/organization/controller/UserController.java
@@ -16,35 +16,6 @@ import java.util.List;
 public class UserController {
     private final UserLogic logic;
 
-    public UserController(UserLogic logic) {
-        this.logic = logic;
-    }
-
-    @DeleteMapping("deleteById/{id}")
-    public void deleteById(@PathVariable("id") String id) {
-        logic.deleteById(id);
-    }
-
-    @GetMapping("getById/{id}")
-    public User getById(@PathVariable("id") String id) {
-        return logic.getById(id);
-    }
-
-    @GetMapping("findByName")
-    public List<User> findByName(@RequestParam("name") String name) {
-        return logic.findByName(name);
-    }
-
-    @GetMapping("hasPermission")
-    public Boolean hasPermission(@RequestParam("name") String name, @RequestParam("category") PermissionCategory category, @RequestParam("type") PermissionType type) {
-        return logic.hasPermission(name, category, type);
-    }
-
-    @PostMapping(value = "save", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public User save(@RequestBody @Valid User user) {
-        return logic.save(user);
-    }
-
     @GetMapping("getUserInfo")
     public UserInfo getUserInfo() {
         return logic.getUserInfo();
