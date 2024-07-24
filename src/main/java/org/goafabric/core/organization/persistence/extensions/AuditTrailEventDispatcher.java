4779c810ef269ba6cd3905298902b7ea86f20296
Andreas Mautsch <amautsch>
2024-07-24 14:14:42 +0200

    changes

--- src/main/java/org/goafabric/core/organization/persistence/extensions/AuditTrailEventDispatcher.java
+++ src/main/java/org/goafabric/core/organization/persistence/extensions/AuditTrailEventDispatcher.java
@@ -6,8 +6,11 @@ import org.goafabric.event.EventData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,10 +22,15 @@ public class AuditTrailEventDispatcher {
 
     private final KafkaTemplate kafkaTemplate;
     private final String kafkaServers;
+    private final RestClient auditRestClient;
+    private final String eventDispatcherUri;
 
-    public AuditTrailEventDispatcher(KafkaTemplate kafkaTemplate, @Value("${spring.kafka.bootstrap-servers:}") String kafkaServers) {
+    public AuditTrailEventDispatcher(KafkaTemplate kafkaTemplate, @Value("${spring.kafka.bootstrap-servers:}") String kafkaServers,
+                                        RestClient.Builder builder, @Value("${event.dispatcher.uri:}") String eventDispatcherUri) {
         this.kafkaTemplate = kafkaTemplate;
         this.kafkaServers = kafkaServers;
+        this.eventDispatcherUri = eventDispatcherUri;
+        this.auditRestClient = createRestClient(builder);
     }
 
     public void dispatchEvent(AuditTrailListener.AuditTrail auditTrail, Object payload) {
@@ -33,7 +41,31 @@ public class AuditTrailEventDispatcher {
                 kafkaTemplate.send(topic, auditTrail.objectId(), new EventData(TenantContext.getAdapterHeaderMap(), auditTrail.objectId(), auditTrail.operation().toString(), payload));
             });
         }
+        if (!eventDispatcherUri.isEmpty()) {
+            var changeEvent = new ChangeEvent(auditTrail.id(), TenantContext.getTenantId(), auditTrail.objectId(), auditTrail.objectType(), auditTrail.operation(), "core", payload);
+            executor.submit(() -> {
+                try {
+                    auditRestClient.post().uri(eventDispatcherUri).contentType(MediaType.APPLICATION_JSON).body(changeEvent).retrieve();
+                } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                }
+            });
+        }
+    }
+    
+
+    private static RestClient createRestClient(RestClient.Builder builder) {
+        var requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(1000);
+        requestFactory.setReadTimeout(1000);
+        return builder.requestInterceptor((request, body, execution) -> {
+                    TenantContext.getAdapterHeaderMap().forEach((key, value) -> request.getHeaders().set(key, value));
+                    return execution.execute(request, body);
+                })
+                .requestFactory(requestFactory)
+                .build();
     }
 
+    record ChangeEvent (String id, String tenantId, String referenceId, String type, AuditTrailListener.DbOperation operation, String origin, Object payload) {}
 
 }
