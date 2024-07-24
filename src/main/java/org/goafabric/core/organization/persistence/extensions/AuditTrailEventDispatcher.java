b33916e942ccaa8203b000e1b888e532f2320680
Andreas Mautsch <amautsch>
2024-07-24 09:47:34 +0200

    changes

--- src/main/java/org/goafabric/core/organization/persistence/extensions/AuditTrailEventDispatcher.java
+++ src/main/java/org/goafabric/core/organization/persistence/extensions/AuditTrailEventDispatcher.java
@@ -1,69 +1,38 @@
 package org.goafabric.core.organization.persistence.extensions;
 
 import org.goafabric.core.extensions.TenantContext;
+import org.goafabric.core.medicalrecords.persistence.jpa.entity.MedicalRecordEo;
 import org.goafabric.event.EventData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Component
-@RegisterReflectionForBinding(AuditTrailEventDispatcher.ChangeEvent.class)
 public class AuditTrailEventDispatcher {
-    private final ExecutorService executor = Executors.newFixedThreadPool(3);
-    private final RestClient auditRestClient;
-    private final String eventDispatcherUri;
+    private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    private KafkaTemplate kafkaTemplate;
+    private final KafkaTemplate kafkaTemplate;
+    private final String kafkaServers;
 
-    @Value("${spring.kafka.bootstrap-servers:}")
-    private String kafkaServers;
-
-    public AuditTrailEventDispatcher(RestClient.Builder builder, @Value("${event.dispatcher.uri:}") String eventDispatcherUri) {
-        this.eventDispatcherUri = eventDispatcherUri;
-        this.auditRestClient = createRestClient(builder);
-    }
-
-    private static RestClient createRestClient(RestClient.Builder builder) {
-        var requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(1000);
-        requestFactory.setReadTimeout(1000);
-        return builder.requestInterceptor((request, body, execution) -> {
-                    TenantContext.getAdapterHeaderMap().forEach((key, value) -> request.getHeaders().set(key, value));
-                    return execution.execute(request, body);
-                })
-                .requestFactory(requestFactory)
-                .build();
+    public AuditTrailEventDispatcher(KafkaTemplate kafkaTemplate, @Value("${spring.kafka.bootstrap-servers:}") String kafkaServers) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.kafkaServers = kafkaServers;
     }
 
     public void dispatchEvent(AuditTrailListener.AuditTrail auditTrail, Object payload) {
         if (!kafkaServers.isEmpty()) {
-            kafkaTemplate.send(auditTrail.objectType(), auditTrail.objectId(), new EventData(TenantContext.getAdapterHeaderMap(), auditTrail.objectId(), auditTrail.operation().toString(), payload));
-        }
-
-        if (!eventDispatcherUri.isEmpty()) {
-            var changeEvent = new ChangeEvent(auditTrail.id(), TenantContext.getTenantId(), auditTrail.objectId(), auditTrail.objectType(), auditTrail.operation(), "core", payload);
             executor.submit(() -> {
-                try {
-                    auditRestClient.post().uri(eventDispatcherUri).contentType(MediaType.APPLICATION_JSON).body(changeEvent).retrieve();
-                } catch (Exception e) {
-                    log.error(e.getMessage(), e);
-                }
+                String topic = payload instanceof MedicalRecordEo medicalRecordEo ? medicalRecordEo.getType().toLowerCase() : auditTrail.objectType();
+                kafkaTemplate.send(topic, auditTrail.objectId(), new EventData(TenantContext.getAdapterHeaderMap(), auditTrail.objectId(), auditTrail.operation().toString(), payload));
             });
         }
     }
 
-    record ChangeEvent (String id, String tenantId, String referenceId, String type, AuditTrailListener.DbOperation operation, String origin, Object payload) {}
-    
+
 }
