3dc0bf6f829efec1fde5c4a3d5b5ce9d2ec4b3f9
Andreas Mautsch <amautsch>
2025-06-16 08:51:11 +0200

    chang

--- src/main/java/org/goafabric/core/fhir/r4/controller/dto/Bundle.java
+++ src/main/java/org/goafabric/core/fhir/r4/controller/dto/Bundle.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Bundle<T> {
     public String id;
-    public final String resourceType = "Bundle";
+    public static final String resourceType = "Bundle";
 
     public List<BundleEntryComponent<T>> entry = new ArrayList<>();
 
