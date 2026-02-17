8fb3d634b136a829e7f3d2da863fcb9ae846c006
Andreas Mautsch <amautsch>
2026-02-17 08:24:19 +0100

    chang

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ plugins {
 	id("net.researchgate.release") version "3.1.0"
 	id("org.sonarqube") version "7.1.0.6387"
 
-	id("org.cyclonedx.bom") version "3.1.0"
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 }
 
